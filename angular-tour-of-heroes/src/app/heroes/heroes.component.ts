4c732246a5213b061aeb09a1027b546acaf98066
IT16029682 <krishan.kr1996@gmail.com>
2018-05-09 23:42:46 +0530

    did Master/Detail

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -13,15 +13,17 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() {
 
    }
 
   ngOnInit() {
   }
+
 
 }
