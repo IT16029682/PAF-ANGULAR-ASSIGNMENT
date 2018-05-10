f5e1ff18f37eade47429caa379a2090094058bc2
IT16029682 <krishan.kr1996@gmail.com>
2018-05-10 15:53:39 +0530

    Observable HeroService and Subscribe in HeroesComponent

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 @Injectable({
@@ -9,7 +9,7 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
 
   constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
